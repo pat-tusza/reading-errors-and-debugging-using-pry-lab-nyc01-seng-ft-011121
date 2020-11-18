fac0b7d752467844554abea60eb5654d562af0b0
Patrick Tuszakowski <mrtall500@gmail.com>
2020-11-18 05:04:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ end
 
 def selection(num)
   if num == 1
-    return "YUM YUM MUNCH MUNCH MUNCH"
+     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-     return "HAM HAM HAM IN MY TUMMY"
+      "HAM HAM HAM IN MY TUMMY"
   end
 end
 
