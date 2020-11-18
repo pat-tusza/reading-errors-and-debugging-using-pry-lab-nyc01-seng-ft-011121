e12922258fdd6a93d6fd658ea88ce1d60772757a
Patrick Tuszakowski <mrtall500@gmail.com>
2020-11-18 04:17:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,10 +14,9 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
+     binding.pry
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
-     binding.pry
-     puts "hello"
   end
 end
 
