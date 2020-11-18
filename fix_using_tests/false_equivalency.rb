32b22905b9ab38896f60e19d2dbe1a422d4e2f4a
Patrick Tuszakowski <mrtall500@gmail.com>
2020-11-18 05:04:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
     return "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      return "HAM HAM HAM IN MY TUMMY"
   end
 end
